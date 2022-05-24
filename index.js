9b8da9920613e995c62aa435e3dbc80abf2b9687
arifvtti <arif.vtti@gmail.com>
2022-05-24 12:56:24 +0600

    make a token and send to client side

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -61,7 +62,10 @@ async function run() {
                 $set: user,
             }
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
+            const token = jwt.sign({ email }, process.env.SECRET_KEY, {
+                expiresIn: "1d"
+            })
+            res.send({ result, token });
         })
     }
     finally {
