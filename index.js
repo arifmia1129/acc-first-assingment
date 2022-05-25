e46996effd2376191871ebeabfe895825cf57b19
arifvtti <arif.vtti@gmail.com>
2022-05-25 09:30:07 +0600

    dynamic send email when user booking product

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,6 +29,59 @@ const verifyJWT = (req, res, next) => {
         req.decoded = decoded;
         next();
     })
+}
+
+const options = {
+    auth: {
+      api_key: process.env.EMAIL_API
+    }
+  }
+const emailSenderClient = nodemailer.createTransport(sgTransport(options));
+
+const bookingConfirmEmail = (bookingInfo) => {
+    const {productId,
+        product,
+        orderQuantity,
+        totalPrice,
+        user,
+        phone,
+        address} = bookingInfo;
+    const email = {
+        from: process.env.EMAIL_SENDER,
+        to: user,
+        subject: `Your order booked for ${product}`,
+        text: `Your order booked for ${product}`,
+        html: 
+        `
+        <div>
+                <h2>Assalamu Alaikum Dear Sir/Mam,</h2>,
+                <h1>We have receive your order.</h1>
+                <h3>Your order details is :</h3>
+                <ul>
+                    <li>Order for: ${product}</li>
+                    <li>Order quantity: ${orderQuantity}</li>
+                    <li>Total price: ${totalPrice}</li>
+                    <li>Contact No: ${phone}</li>
+                    <li>Address: ${address}</li>
+                </ul>
+                <p>If you want confirm order please go to Dashboard panel and make sure payment.</p>
+                <h2>Thanks for your order.</h2>
+                <h2>Stay with us!</h2>
+                <br />
+                <br />
+                <h1>A&B Group of Industries Ltd.</h1>
+            </div>
+        `
+      };
+      
+      emailSenderClient.sendMail(email, function(err, info){
+          if (err ){
+            console.log(err);
+          }
+          else {
+            console.log('Message sent: ', info);
+          }
+      });
 }
 
 
@@ -73,6 +129,7 @@ async function run() {
         app.post("/booking", async (req, res) => {
             const bookingInfo = req.body;
             const result = await bookingCollection.insertOne(bookingInfo);
+            bookingConfirmEmail(bookingInfo);
             res.send(result);
         })
         app.delete("/booking/:id", async (req, res) => {
