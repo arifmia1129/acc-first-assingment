7e4f3a1d6fed6c96ad284be0c2a0d2230a85d8c6
arifvtti <arif.vtti@gmail.com>
2022-06-04 10:14:33 +0600

    cors issue

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: '*' }))
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With , Content-Type, Accept");
+    next();
+});
+// app.use(cors({ origin: '*' }))
 app.use(express.json());
 
 
