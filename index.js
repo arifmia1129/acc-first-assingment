a8e317d7c363b23d3521f19cc6f24e03566c7947
arifvtti <arif.vtti@gmail.com>
2022-06-04 10:24:54 +0600

    cors issue

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const port = process.env.PORT || 5000;
 
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With , Content-Type, Accept");
     next();
 });
-app.use(cors());
-app.use(cors({ origin: '*' }))
+// app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' }))
 app.use(express.json());
 
 
