94133129eae227186ecdb05b190385487807e100
arifvtti <arif.vtti@gmail.com>
2022-06-04 19:57:03 +0600

    cors

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.use(cors({ origin: '*' }))
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
