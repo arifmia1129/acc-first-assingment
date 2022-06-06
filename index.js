f8c37c28fc8848d93ec103c23e248774aea2020c
arifvtti <arif.vtti@gmail.com>
2022-06-06 09:49:36 +0600

    msg

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://a-b-group.web.app");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
